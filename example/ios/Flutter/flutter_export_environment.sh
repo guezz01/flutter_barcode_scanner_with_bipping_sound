9a3bdee5cbdd734aa6fc1b83b6e76cf6bfad9042
Aymen Guezmir <aymen.guezmir@saphir.local>
2024-08-14 09:41:03 +0100

    update sdk and version

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\aymen.guezmir\Desktop\cutomized packages flutter\flutter_barcode_scanner_with_bipping_sound\example"
+export "FLUTTER_APPLICATION_PATH=D:\Desktop Aymen\cutomized packages flutter\flutter_barcode_scanner_with_bipping_sound\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
